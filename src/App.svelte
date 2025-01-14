5b55c0d846e5d3aec1d8a8bda366f124bfb97611
Nicolas Charlot <nicolas.charlot@isics.fr>
2025-01-14 20:53:32 +0100

    Remove styles and use Bootstrap 3 collapse layout

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,8 @@
   import type { Theme } from "./lib/types/Theme";
   import type { Question } from "./lib/types/Question";
   import Accordion from "./lib/components/Accordion.svelte";
-  import { t, locale, locales } from "./lib/stores/i18n";
-  import "./app.css";
+
+  export let locale = 'fr';
 
   let themes: Theme[] = [];
   let questions: Question[] = [];
@@ -26,36 +26,14 @@
   });
 </script>
 
-<main class="container mx-auto p-4">
-  <p>
-    <select bind:value={$locale} class="text-gray-800 bg-white">
-      {#each locales as l}
-        <option value={l} class="text-gray-800 bg-white">
-          {l.toUpperCase()}
-        </option>
-      {/each}
-    </select>
-  </p>
+<main>
+  {#each themes as theme (theme.id)}
+    <h3>
+      {locale === "fr" ? theme.name_fr : theme.name_en}
+    </h3>
 
-  <h2 class="text-5xl text-gray-800 font-title text-center">
-    {$t("faqTitle")}
-  </h2>
-
-  <h3 class="text-2xl text-gray-400 uppercase font-black text-center">
-    {$t("faqSubtitle")}
-  </h3>
-
-  <div class="flex flex-col items-center">
-    {#each themes as theme (theme.id)}
-      <div class="my-4 w-full xl:w-5/6 2xl:w-3/4 mx-auto">
-        <h4 class="text-3xl font-bold text-center my-4 w-full">
-          {$locale === "fr" ? theme.name_fr : theme.name_en}
-        </h4>
-
-        <Accordion
-          questions={questions.filter((q) => q.theme_id === theme.id)}
-        />
-      </div>
-    {/each}
-  </div>
+    <div class="panel-group" id="faq-theme{theme.id}" role="tablist" aria-multiselectable="true">
+      <Accordion {locale} questions={questions.filter((q) => q.theme_id === theme.id)}/>
+    </div>
+  {/each}
 </main>
--- src/lib/components/Accordion.svelte
+++ src/lib/components/Accordion.svelte
@@ -1,71 +1,23 @@
 <script lang="ts">
   import type { Question } from "../types/Question";
-  import { locale } from "../stores/i18n";
 
+  export let locale = 'fr';
   export let questions: Question[] = [];
-
-  function toggleAccordion(index: number) {
-    const content = document.getElementById(`content-${index}`);
-    const icon = document.getElementById(`icon-${index}`);
-
-    const downSVG = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
-        <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-      </svg>
-    `;
-
-    const upSVG = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
-        <path fill-rule="evenodd" d="M11.78 9.78a.75.75 0 0 1-1.06 0L8 7.06 5.28 9.78a.75.75 0 0 1-1.06-1.06l3.25-3.25a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06Z" clip-rule="evenodd" />
-      </svg>
-    `;
-
-    if (content && icon) {
-      if (content.style.maxHeight && content.style.maxHeight !== "0px") {
-        content.style.maxHeight = "0";
-        icon.innerHTML = downSVG;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-        icon.innerHTML = upSVG;
-      }
-    }
-  }
 </script>
 
 {#each questions as question (question.id)}
-  <div class="border-b border-slate-200">
-    <button
-      on:click={() => toggleAccordion(question.id)}
-      class="w-full flex justify-between items-center py-5 text-slate-800"
-    >
-      <span class="text-xl font-bold">
-        {$locale === "fr" ? question.question_fr : question.question_en}
-      </span>
-      <span
-        id="icon-{question.id}"
-        class="text-slate-800 transition-transform duration-300"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          class="w-4 h-4"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </span>
-    </button>
-    <div
-      id="content-{question.id}"
-      class="max-h-0 overflow-hidden transition-all duration-300 ease-in-out"
-    >
-      <p class="pb-5 text-lg text-slate-500">
-        {$locale === "fr" ? question.answer_fr : question.answer_en}
-      </p>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="faq-question{question.id}">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#faq-theme{question.theme_id}" href="#faq-answer{question.id}" aria-expanded="true" aria-controls="faq-answer{question.id}">
+          {locale === "fr" ? question.question_fr : question.question_en}
+        </a>
+      </h4>
+    </div>
+    <div id="faq-answer{question.id}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="faq-question{question.id}">
+      <div class="panel-body">
+        {locale === "fr" ? question.answer_fr : question.answer_en}
+      </div>
     </div>
   </div>
 {/each}
